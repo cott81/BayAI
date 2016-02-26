25db15dbd320eb09f632c7ef47226a342f13d95a
Dominik Kirchner <kirchner.dominik@googlemail.com>
2016-02-26 22:40:09 +0100

    corrected build bugs

---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+  using std::cerr;
+  using std::cout;
+  using std::endl;
+
+
+int main(int    argc, char** argv)
+{
+    int result = 0;
+
+    std::cout <<"1"<< std::endl;
+    std::cout <<"2"<< std::endl;
+    std::cout <<"3"<< std::endl;
+
+
+
+    return result;
+}
+
