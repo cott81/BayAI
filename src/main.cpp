85d271acbba036c5b4fb68681fd0288b0a71b205
Dominik Kirchner <kirchner.dominik@googlemail.com>
2018-04-14 10:08:52 +0200

    further code clean up

--- src/main.cpp
+++ src/main.cpp
@@ -1,53 +1,53 @@
 #include <iostream>
 #include "Graph.h"
 
-  using std::cerr;
-  using std::cout;
-  using std::endl;
+using std::cerr;
+using std::cout;
+using std::endl;
 
 #include <tuple>
 #include <vector>
 
-  graph::Graph generate(void);
+graph::Graph generate(void);
 
 graph::Graph generate(void)
 {
 
-    graph::Graph g(graph::ADJACENCY_LIST);
+  graph::Graph g(graph::ADJACENCY_LIST);
 
-    int idNodeA = g.AddVertex();
-    int idNodeB = g.AddVertex();
-    int idNodeC = g.AddVertex();
-    int idNodeD = g.AddVertex();
+  int idNodeA = g.AddVertex();
+  int idNodeB = g.AddVertex();
+  int idNodeC = g.AddVertex();
+  int idNodeD = g.AddVertex();
 
-    g.PrintVertices();
+  g.PrintVertices();
 
-    g.AddEdge(idNodeA, idNodeB);
-    g.AddEdge(idNodeA, idNodeC);
-    g.AddEdge(idNodeB, idNodeC);
-    g.AddEdge(idNodeC, idNodeD);
+  g.AddEdge(idNodeA, idNodeB);
+  g.AddEdge(idNodeA, idNodeC);
+  g.AddEdge(idNodeB, idNodeC);
+  g.AddEdge(idNodeC, idNodeD);
 
-    g.PrintEdges();
+  g.PrintEdges();
 
-    g.GetVertex(idNodeA)->PrintOutEdges();
-    cout << g.GetVertex(idNodeA)->GetOutEdges()[1]->GetId() << endl;
+  g.GetVertex(idNodeA)->PrintOutEdges();
+  cout << g.GetVertex(idNodeA)->GetOutEdges()[1]->GetId() << endl;
 
-    return g;
+  return g;
 }
 
 int main(int argc, char **argv)
 {
-	cout << "/*" << endl; // start comment for DOT
-    cout << "Test Application: ..." << endl;
+  cout << "/*" << endl; // start comment for DOT
+  cout << "Test Application: ..." << endl;
 
-    // generate data structure ...
-    graph::Graph g = generate();
+  // generate data structure ...
+  graph::Graph g = generate();
 
-    cout << "*/" << endl; //end comment in DOT
+  cout << "*/" << endl; //end comment in DOT
 
-    cout << g.GenerateDOTDescription() << std::endl;
+  cout << g.GenerateDOTDescription() << std::endl;
 
-    g.VisualizeDOTGraph("test");
+  g.VisualizeDOTGraph("test");
 
-    return 0;
+  return 0;
 }
