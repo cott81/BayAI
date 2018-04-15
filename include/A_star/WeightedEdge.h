da97522eccc740b177771a0ad5ebef48022797cc
Dominik Kirchner <kirchner.dominik@googlemail.com>
2018-04-15 14:02:14 +0200

    ... headers of a star

---
+++ include/A_star/WeightedEdge.h
@@ -0,0 +1,30 @@
+/*
+ * Link.h
+ *
+ * Copyright Feb 26, 2016 Dominik Kirchner
+ */
+
+#ifndef WEIGHTEDEDGE_H_
+#define WEIGHTEDEDGE_H_
+
+#include "Edge.h"
+#include "Vertex.h"
+
+namespace a_star {
+
+class WeightedEdge: public graph::Edge {
+public:
+	WeightedEdge(graph::Vertex* origin, graph::Vertex* goal, double edgeWeight);
+	virtual ~WeightedEdge();
+
+	unsigned int AddWeight(double edgeWeight);
+	double GetWeight(void) {return this->weight;};
+
+protected:
+	double weight;
+
+};
+
+} /* namespace a_star */
+
+#endif /* WEIGHTEDEDGE_H_ */
