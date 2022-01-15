9aae5dff94447b331b387381fea5c61c7721c71e
Dominik Kirchner <kirchner.dominik@googlemail.com>
2022-01-15 21:21:12 +0000

    minor code clean up in unit tests

--- test/tests.cpp
+++ test/tests.cpp
@@ -2,19 +2,8 @@
 
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
- 
-/*
-TEST(GraphTest, TestTestFunc)
-{
-	EXPECT_EQ(1,1);
-	ASSERT_EQ(1,1);
-}
-*/
 
 
-
-
- 
 int main(int argc, char **argv) {
     //testing::InitGoogleTest(&argc, argv);
     testing::InitGoogleMock(&argc, argv);
